2eb95a291143efbb9754aa3b0f120a02ddb626ed
vhhvf <122591347+vhhvf@users.noreply.github.com>
2023-01-16 23:03:32 +0300

    Update gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -1,12 +1,12 @@
 import fs from 'fs'
 let handler = async (m, { conn, args }) => {
 let group = m.chat
-conn.reply(m.chat, 'لينك الجروب♥✨: https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
+conn.reply(m.chat, 'لينك الجروب♥✨: https://chat.whatsapp.com/JiyXKzifaoTJCi4Ga9KIlI .com/' + await conn.groupInviteCode(group), m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: 'لينك الجروب♥✨',
 body: 'THE ELGAZAR-BOT',         
 previewType: 0, thumbnail: fs.readFileSync("./Menu2.jpg"),
-sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})   
+sourceUrl: `https://github.com/vhhvf/TheMystic-Bot-MD-2`}}})   
 }
 handler.help = ['لينك']
 handler.tags = ['group']
